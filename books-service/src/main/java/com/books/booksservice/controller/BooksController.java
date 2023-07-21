ab9ad839abf9129fe68feae5c6a7129bfd04ae83
Shili Chen <v-shilichen@microsoft.com>
2023-07-21 10:44:25 +0800

    Update for consumption plan

--- books-service/src/main/java/com/books/booksservice/controller/BooksController.java
+++ books-service/src/main/java/com/books/booksservice/controller/BooksController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.books.booksservice.data.model.Book;
@@ -13,21 +14,20 @@ import com.books.booksservice.service.BooksService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping("/books")
 @RequiredArgsConstructor
 public class BooksController{
     private final BooksService booksService;
 
-
     @PreAuthorize("hasAuthority('APPROLE_Books.Read')")
-    @GetMapping("books/{id}")
+    @GetMapping("{id}")
     public Book getBook(@PathVariable Long id){
         return booksService.getBook(id);
     }
 
     @PreAuthorize("hasAuthority('APPROLE_Books.Write')")
-    @PostMapping("/books/add")
+    @PostMapping("/add")
     public Book addBook(@RequestBody Book book){
         return booksService.saveBook(book);
     }
-
 }
